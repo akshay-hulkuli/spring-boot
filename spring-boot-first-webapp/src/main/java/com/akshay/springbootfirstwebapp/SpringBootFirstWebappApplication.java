3341177783d56273e334d26d45c63ba4cd0dc4da
Akshaya HP <akshayhulkuli@gmail.com>
2024-04-27 13:11:00 +0530

    saving changes

--- spring-boot-first-webapp/src/main/java/com/akshay/springbootfirstwebapp/SpringBootFirstWebappApplication.java
+++ spring-boot-first-webapp/src/main/java/com/akshay/springbootfirstwebapp/SpringBootFirstWebappApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootFirstWebappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootFirstWebappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFirstWebappApplication.class, args);
+    }
 
 }
