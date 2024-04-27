249a247c636f5e692f9b245db9e5395078916a2d
Akshaya HP <akshayhulkuli@gmail.com>
2024-04-27 13:03:12 +0530

    saving changes

---
+++ spring-boot-first-webapp/src/main/java/com/akshay/springbootfirstwebapp/SpringBootFirstWebappApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.springbootfirstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstWebappApplication.class, args);
+	}
+
+}
---
+++ spring-boot-first-webapp/src/test/java/com/akshay/springbootfirstwebapp/SpringBootFirstWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.springbootfirstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
