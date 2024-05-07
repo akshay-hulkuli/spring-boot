89b7b8e8bc417b3777ccf04434d4b84317447722
Akshaya HP <akshayhulkuli@gmail.com>
2024-05-07 09:27:21 +0530

    saving changes

---
+++ spring-boot-restful-web-services/src/main/java/com/akshay/springbootrestfulwebservices/hello/HelloWorldController.java
@@ -0,0 +1,41 @@
+package com.akshay.springbootrestfulwebservices.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    /*
+        When we say @Controller we are more focused into MVC.
+        The controller is expected to return VIEW / JSP.
+        But when we say @RestController we are dealing with Json data mostly.
+        To return string data we need to explicitly specify @RequestBody in case of @Controller
+     */
+
+    /*
+        Spring Boot has 2 important features 1. starter project and Auto configuration.
+        Starter project spring-boot-starter-web provides Spring-MVC, tomcat, Jackson for JSON conversion etc.
+        Auto-configuration by checking what are the classes available auto-configures things.
+        It configures DispatcherServlet, @RequestBody, required json converter, Error Mapping (error page), tomcat server etc.
+     */
+
+
+    //@RequestMapping(path = "/hello-world", method = RequestMethod.GET)
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("hello-world");
+    }
+
+    // Path parameters
+    @GetMapping("hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World %s", name));
+    }
+}
