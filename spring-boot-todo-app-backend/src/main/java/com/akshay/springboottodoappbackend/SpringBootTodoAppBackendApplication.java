308879dbbcc59f9a2f6e59c0d57a109c13cba2c3
Akshaya HP <akshayhulkuli@gmail.com>
2024-06-02 18:35:43 +0530

    initialised module

---
+++ spring-boot-todo-app-backend/src/main/java/com/akshay/springboottodoappbackend/SpringBootTodoAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.springboottodoappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodoAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTodoAppBackendApplication.class, args);
+    }
+
+}
